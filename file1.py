105202c1f0d0796d4e707c0552515b76aa311648
ibrahim <you@example.com>
2023-09-24 14:41:13 +0530

    adding some features

--- file1.py
+++ file1.py
@@ -2,11 +2,20 @@ a= 0
 b= 1
 number=int(input("enter the number :"))
 
-print(a)
-print(b)
+if number == 1:
+  print(a)
 
-for i in range(2,number):
-    c = a + b
-    print(c)
-    a = b
-    b = c
+elif number == 2:
+  print(a)
+  print(b)
+  
+elif number <= 0:
+   print("invalid input")
+
+else:
+     for i in range(2,number):
+        c = a + b
+        print(c)
+        a = b
+        b = c
+    
