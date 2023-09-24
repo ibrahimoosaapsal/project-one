355ed459db588d1fca46a1bbf9265e8deee13a65
ibrahim <you@example.com>
2023-09-24 21:51:01 +0530

    second problem

--- file2.py
+++ file2.py
@@ -6,3 +6,14 @@ address=input("enter your address :")
 print("My name is :",name)
 print("My age is :",age)
 print("my address :",address)
+
+#variable sum
+a=int(input("enter A value :"))
+b=int(input("enter b value :"))
+c=int(input("enter C value :"))
+d=a*b*c
+e=a+b+c
+f=d/e
+print("multiplied value of three values :",d)
+print("adding value of three values :",e)
+print("divisional value of adding add multiply value :",f)
