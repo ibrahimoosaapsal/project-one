17e60edaec584eaf081daca96c76f9384c4a1783
ibrahim <you@example.com>
2023-09-24 22:09:49 +0530

    casting and input is over

--- file2.py
+++ file2.py
@@ -17,3 +17,14 @@ f=d/e
 print("multiplied value of three values :",d)
 print("adding value of three values :",e)
 print("divisional value of adding add multiply value :",f)
+#third problem
+name=input("enter the name :")
+score=int(input("enter your score :"))
+department=input("enter your department :")
+if score<=100:
+  divide=score/10
+else:
+     print("enter the valid score ???")
+print("your name :",name)
+print("your score",divide,"/10")
+print("your depaartment",department)
