38d04224af8412772501a9a24fe5e8bf7c57281d
ibrahim <you@example.com>
2023-09-24 21:37:21 +0530

    input program

--- file2.py
+++ file2.py
@@ -1,3 +1,8 @@
-a=20
-b=30
-print(a+b)
+#get input from user
+name=input("enter your name :")
+age=int(input("enter your age :"))
+address=input("enter your address :")
+
+print("My name is :",name)
+print("My age is :",age)
+print("my address :",address)
