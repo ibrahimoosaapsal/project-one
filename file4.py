5bf368abb41b1b3df4d3b501664b56c22efa5112
ibrahim <you@example.com>
2023-09-26 13:12:44 +0530

    file updated

---
+++ file4.py
@@ -0,0 +1,28 @@
+"""#print the table:-
+
+j=int(input("enter the number"))
+
+for i in range(1,11):
+    print(j,"x",i,"=",i*j)
+
+    #second problem
+a=int(input("enter a value :"))
+b=int(input("enter b value :"))
+
+for i in range(a+1,b):
+    print(i)
+
+#third problem
+
+for i in range(1,11):
+    if(i%2==0):
+        print(i)"""
+
+#forth problem
+
+a=0
+
+for i in range(1,11):
+    if(i%2==1):
+       a=a+1
+       print(a)
