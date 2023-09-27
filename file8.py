a00e3739d1e16f260845d8d240c1c97219e0c574
ibrahim <you@example.com>
2023-09-27 15:13:27 +0530

    new message

--- file8.py
+++ file8.py
@@ -3,3 +3,20 @@ def driver():
     print("Driving")
 
 driver()
+
+#first problem
+
+def add():
+    print(a+b)
+def sub():
+    print(a-b)
+def mul():
+    print(a*b)
+def div():
+    print(a/b)
+a=int(input("enter a value :"))
+b=int(input("enter b value :"))
+add()
+sub()
+mul()
+div()
