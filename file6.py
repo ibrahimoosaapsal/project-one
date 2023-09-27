2147887d7e7ab372d5e82cae641313f7df563dba
ibrahim <you@example.com>
2023-09-27 06:41:59 +0530

    file update....

--- file6.py
+++ file6.py
@@ -1,1 +1,39 @@
-print("i")
+"""#first problem:-
+i=10
+
+while(i<=200):
+    print(i,end=",")
+    i=i+10
+
+#second problem:-
+i=10
+
+while(i>=0):
+    print(i)
+    i=i-1"""
+
+#third problem
+
+i=int(input("ehter the number :"))
+fact=1
+
+while(i>0):
+    fact=fact*i
+    i=i-1
+
+print(fact)
+
+#forth program
+
+a=[]
+sum=0
+
+for i in range(10):
+    c=int(input("enter value no"+str(i+1)+":"))
+    a.append(c)
+
+for i in a:
+    sum=sum+i
+
+print(sum)
+print(sum/10)
