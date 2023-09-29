3774ffce0b7a2e3f9e511cfa759940522dd27a90
ibrahim <you@example.com>
2023-09-29 09:32:16 +0530

    file added

---
+++ file9.py
@@ -0,0 +1,20 @@
+#classes and objects
+#classes is nothing bur set of functions 
+class coding:#this is class
+    name="unname"
+    age=123
+    def c():#function 1
+     print("start coding")
+    def java():#function 2
+       print("stat coding with java")
+    def python():#function 3
+       print("start coding with python")
+
+ayan=coding
+apsal=coding
+apsal.python()#it will call the function 3
+
+ayan.name="ayan"
+apsal.name="ibrahim"#to assign this value to that empty name variable
+print(apsal.name)
+print(ayan.name)
