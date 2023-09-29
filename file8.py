7606427923c07cb513fd21675b42342c302c22c8
ibrahim <you@example.com>
2023-09-29 08:52:51 +0530

    file enclosed

--- file8.py
+++ file8.py
@@ -20,3 +20,80 @@ add()
 sub()
 mul()
 div()
+
+#second problem
+
+def er(ayan):
+    print("hello"+ayan)
+
+er() 
+
+#third problem
+
+def findevenorodd(num=int(input("enter your number :"))):
+    if(num==1):
+        print("odd number")
+    else:
+        print("even number")
+
+findevenorodd()
+
+#forth problem
+
+def result(num=int(input("enter your mark :"))):
+    if(num>35):
+        print("pass")
+    else:
+        print("fail")
+result()
+
+#fifth problem
+
+def findrange(
+     num=int(input("enter first num :")),
+     num2=int(input("enter your num 2 :"))):
+     for i in range(num,num2):
+      print(i)
+
+findrange()
+
+#sixth program
+u_name="apsal"
+password=123
+
+a=input("enter your name :")
+b=int(input("enter your password :"))
+
+def validate():
+    if(a==u_name and b==password):
+        return 'corect'
+    else:
+        return 'wrong' 
+
+c=validate()
+print(c)
+
+#seventh program
+a=int(input("enter a value :")) 
+b=int(input("enter b value :"))
+c=int(input("enter c value :"))
+
+def add():
+    return a+b
+
+d=add()
+print(d*c)
+
+#eighth program
+
+def add(n1,n2):
+    return n1+n2
+
+a=int(input("enter a :"))
+b=int(input("enter b :"))
+c=int(input("enter c :"))
+
+added=add(a,b)
+output=added*c
+print(output)
+
